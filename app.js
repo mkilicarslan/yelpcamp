42f2a3ab304679f5df1e8f198da82f9226c49606
Muhammed K <mka281@icloud.com>
2017-12-11 12:28:05 +0000

    add mLab

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ var commentRoutes = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes = require("./routes/index");
 
-mongoose.connect("mongodb://localhost/yelp_camp_v10", {useMongoClient:true});
+//mongoose.connect("mongodb://localhost/yelp_camp_v10", {useMongoClient:true});
+mongoose.connect("mongodb://admin:yelp.camp@ds137256.mlab.com:37256/yelpcamp", {useMongoClient:true});
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname+"/public"));
